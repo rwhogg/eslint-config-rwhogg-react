43f8eb6bca83fa325de6ece4c7850df756a7a571
Bob Wombat Hogg <wombat@rwhogg.site>
2020-06-07 13:07:09 -0400

    Reactify

--- index.js
+++ index.js
@@ -3,8 +3,28 @@ module.exports = {
         browser: true,
         es6: true,
     },
-    extends: "rwhogg",
-    rules: {
-
+    extends: [
+        "rwhogg",
+        "plugin:react/recommended"
+    ],
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true,
+        },
+    },
+    "env": {
+        "browser": true,
+        "es2020": true,
+        "jest": true,
+    },
+    "settings": {
+        "react": {
+            "version": "detect",
+        },
+    },
+    "rules": {
+        "consistent-return": 0,
     },
 }
